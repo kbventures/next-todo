034b21e5e92459946dabbb3b0ae3f6a5199c9c46
kbventures <kmb_x@hotmail.com>
2023-05-13 11:41:54 -0600

    Added Home Api

---
+++ pages/homes.ts
@@ -0,0 +1,35 @@
+import prisma from '@/lib/prisma'
+import { NextApiRequest, NextApiResponse } from 'next';
+
+
+
+export default async function handler(req: NextApiRequest, res:NextApiResponse) {
+    if (req.method === 'POST') {
+      try {
+        const { image, title, description, price, guests, beds, baths } =
+          req.body;
+        // Imaage Variable in Homes Api
+        console.log("Image Variable In Home Api",image)
+        const home = await prisma.home.create({
+          data: {
+            image,
+            title,
+            description,
+            price,
+            guests,
+            beds,
+            baths,
+          },
+        });
+        res.status(200).json(home);
+      } catch (e) {
+        res.status(500).json({ message: 'Something went wrong' });
+      }
+    }
+    else {
+      res.setHeader('Allow', ['POST']);
+      res
+        .status(405)
+        .json({ message: `HTTP method ${req.method} is not supported.` });
+    }
+  }
